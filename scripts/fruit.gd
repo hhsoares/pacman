6762343914004e5d6102976cc7168c2e3f3c6f18
Hugo Soares <hugos2000@protonmail.com>
2025-11-07 16:29:11 -0300

    added fruit despawn timer

--- scripts/fruit.gd
+++ scripts/fruit.gd
@@ -5,6 +5,12 @@ class_name Fruit
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
+	var t := Timer.new()
+	t.wait_time = randf_range(9.0, 10.0)
+	t.one_shot = true
+	t.timeout.connect(queue_free)
+	add_child(t)
+	t.start()
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Pacman:
