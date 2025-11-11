22deb4d5affaf44cb8785f7930e8d9482c75bff0
Hugo Soares <hugos2000@protonmail.com>
2025-11-11 12:48:43 -0300

    added blinky direction detection

--- scripts/ghosts/blinky.gd
+++ scripts/ghosts/blinky.gd
@@ -2,3 +2,4 @@ extends CharacterBody2D
 class_name Blinky
 
 @export var speed: float = 80.0
+var direction: Vector2 = Vector2.LEFT
--- scripts/ghosts/scatter.gd
+++ scripts/ghosts/scatter.gd
@@ -1,18 +1,31 @@
 extends State
 
 @onready var ghost := owner as CharacterBody2D
+@onready var maze: TileMapLayer = $"../../../Maze"
 
 const TILE_SIZE := 24.0
 const BLINKY_TARGET_TILE := Vector2(12, -14)
+const TILE_CENTER_TOLERANCE := 1.0
 
 func physics_update(delta: float) -> void:
 	if not Globals.ghosts_can_move:
 		return
 
 	if ghost is Blinky:
-		_move_to_target(BLINKY_TARGET_TILE * TILE_SIZE, delta)
+		var cell := _get_current_cell()
+		var adj := _get_adjacent_cells(cell)
+		print("Blinky cell: ", cell)
+		print(adj)
 
-func _move_to_target(target_pos: Vector2, delta: float) -> void:
-	var direction := (target_pos - ghost.global_position).normalized()
-	ghost.velocity = direction * ghost.speed
-	ghost.move_and_slide()
+func _get_current_cell() -> Vector2i:
+	var local_pos: Vector2 = maze.to_local(ghost.global_position)
+	var cell: Vector2i = maze.local_to_map(local_pos)
+	return cell
+
+func _get_adjacent_cells(cell: Vector2i) -> Dictionary:
+	return {
+		"up": cell + Vector2i(0, -1),
+		"down": cell + Vector2i(0, 1),
+		"left": cell + Vector2i(-1, 0),
+		"right": cell + Vector2i(1, 0),
+	}
