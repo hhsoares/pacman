2648a6d258d35e5b20f735040761effc1f063e65
Hugo Soares <hugos2000@protonmail.com>
2025-11-11 10:51:37 -0300

    added basic state machine (without states)

---
+++ scripts/ghosts/state_machine/state.gd
@@ -0,0 +1,15 @@
+class_name State extends Node
+
+var state_machine: StateMachine
+
+func enter() -> void:
+	pass
+
+func exit() -> void:
+	pass
+
+func update(_delta: float) -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	pass
