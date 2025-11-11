2d267528433245b3fd37ef9b615309c974c2214e
Hugo Soares <hugos2000@protonmail.com>
2025-11-11 11:57:16 -0300

    added basic states without function

---
+++ scripts/ghosts/state_machine/start.gd
@@ -0,0 +1,9 @@
+extends State
+
+@onready var ghost := owner as CharacterBody2D
+
+func physics_update(delta: float) -> void:
+	if ghost is Blinky:
+		state_machine.change_state("scatter")
+	if ghost is Pinky:
+		print("pinky start")
