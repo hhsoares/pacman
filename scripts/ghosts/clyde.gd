260f0c26c9b88c066c2211e5761c1f614ac22ac3
Hugo Soares <hugos2000@protonmail.com>
2025-11-12 09:03:45 -0300

    added inky and clyde animations

---
+++ scripts/ghosts/clyde.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+class_name Clyde
+
+@onready var _animated_sprite = $AnimatedSprite2D
+
+@export var speed: float = 100.0
+var direction: Vector2 = Vector2.UP
+
+func _physics_process(delta: float) -> void:
+	if not Globals.ghosts_can_move:
+		_animated_sprite.play("stop")
+		return
+
+	if direction == Vector2.LEFT:
+		_animated_sprite.play("left")
+	elif direction == Vector2.RIGHT:
+		_animated_sprite.play("right")
+	elif direction == Vector2.UP:
+		_animated_sprite.play("up")
+	elif direction == Vector2.DOWN:
+		_animated_sprite.play("down")
